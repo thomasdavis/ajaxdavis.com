28b94e530900747f7faa7b0b991c57d8fe4f9a10
Thomas Davis <thomasalwyndavis@gmail.com>
2017-04-14 01:55:26 +1000

    Blah

--- pages/_document.js
+++ pages/_document.js
@@ -10,14 +10,41 @@ export default class MyDocument extends Document {
     return (
      <html>
        <Head>
+          <title>Thomas "Ajax" Davis, Web Developer, Melbourne</title>
           <link href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" rel="stylesheet" />
           <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/6.0.0/normalize.min.css" rel="stylesheet" />
           <link href="/static/main.css" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Asul:400,700|Open+Sans:400,400i,700,700i" rel="stylesheet" />
        </Head>
        <body className="custom_class">
+       <div className="header">
+         <div className="wrap">
+         <h1>Thomas "Ajax" Davis</h1>
+         </div>
+
+         </div>
+         <div className="wrap">
+         <h2>Web Developer - Melbourne</h2>
+         </div>
+
+       <div className="menu">
+         <div className="wrap">
+           <a href="/">Home</a>
+           <a href="/blog">Blog</a>
+           <a href="/experiments">Experiments</a>
+         </div>
+       </div>
          {this.props.customValue}
          <Main />
+         <div className="wrap">
+         <h3>Contact</h3>
+         <a href="https://twitter.com/ajaxdavis">twitter.com/ajaxdavis</a>
+         <br />
+         <a href="https://github.com/thomasdavis">github.com/thomasdavis</a>
+         <br />
+         <p>Currently not looking for work, but I am looking to hire! -> <a href="https://listium.com/jobs">listium.com/jobs</a></p>
+         </div>
+
          <NextScript />
        </body>
      </html>
---
+++ pages/experiments.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div className="wrap container-fluid">
+    <p>Coming soon...</p>
+  </div>
+)
